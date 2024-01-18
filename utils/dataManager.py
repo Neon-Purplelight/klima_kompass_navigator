816d82bb09528c275b7297c135e188e1d8db3e7c
NeonPurplelight <timsahre@gmx.de>
2024-01-18 15:44:13 +0100

    Aktualisierung der requierements.txt

--- utils/dataManager.py
+++ utils/dataManager.py
@@ -61,3 +61,79 @@ def preprocess_netcdf_data(pathToFile: str):
 # def preprocess_temperature_data(df_temp):
 #     df_temp['mov_avg'] = df_temp['JJA'].rolling(20).mean()
 #     return df_temp
+
+# Read netCDF4 structure
+# import netCDF4 as nc
+
+# # Öffnen der NetCDF-Datei
+# file_path = 'SMI_Gesamtboden_monatlich.nc'
+# dataset = nc.Dataset(file_path, 'r')
+
+# # Auflisten aller Dimensionen
+# print("Dimensionen:", dataset.dimensions.keys())
+
+# # Auflisten aller Variablen und deren Attribute
+# print("Variablen:")
+# for var in dataset.variables:
+#     print(var, dataset.variables[var])
+
+# # Schließen der Datei
+# dataset.close()
+
+
+# Filter out irrelevant months to reduce the file size
+# import netCDF4 as nc
+# import numpy as np
+
+# def remove_months(input_file, output_file):
+#     with nc.Dataset(input_file, 'r') as src, nc.Dataset(output_file, 'w', format='NETCDF4') as dst:
+#         # Zeitvariable extrahieren
+#         time_var = src['time']
+#         time_units = time_var.units
+#         time_calendar = time_var.calendar
+
+#         # Konvertieren der Zeittage in tatsächliche Daten
+#         dates = nc.num2date(time_var[:], units=time_units, calendar=time_calendar)
+
+#         # Indizes der zu behaltenden Daten bestimmen
+#         keep_indices = [i for i, date in enumerate(dates) if date.month not in [1, 2, 3, 11, 12]]
+
+#         # Kopieren der Dimensionen (außer 'time')
+#         for name, dimension in src.dimensions.items():
+#             if name == 'time':
+#                 dst.createDimension(name, len(keep_indices))
+#             else:
+#                 dst.createDimension(name, len(dimension))
+
+#         # Kopieren der Variablen mit Komprimierung
+#         for name, variable in src.variables.items():
+#             # Attribute kopieren, außer _FillValue
+#             attrs = {k: variable.getncattr(k) for k in variable.ncattrs() if k != '_FillValue'}
+
+#             # Erstellen der Variablen mit Komprimierung
+#             if 'time' in variable.dimensions:
+#                 # Falls 'time' in den Dimensionen ist, Filter anwenden
+#                 new_var = dst.createVariable(name, variable.datatype, variable.dimensions, zlib=True, complevel=4)
+#                 if name == 'time':
+#                     # Für die Zeitvariable müssen die Ordnungszahlen der Daten verwendet werden
+#                     new_var[:] = np.array([dates[i].toordinal() for i in keep_indices])
+#                 else:
+#                     # Für alle anderen Variablen, die Zeit enthalten, die gefilterten Daten anwenden
+#                     new_var[:] = variable[keep_indices]
+#             else:
+#                 # Keine Komprimierung notwendig, wenn die Variable nicht zeitabhängig ist
+#                 new_var = dst.createVariable(name, variable.datatype, variable.dimensions)
+#                 new_var[:] = variable[:]
+
+#             # Attribute zuweisen, nachdem die Daten zugewiesen wurden
+#             new_var.setncatts(attrs)
+
+#     print(f'Gefilterte NetCDF-Datei gespeichert als: {output_file}')
+
+# # Pfade zur ursprünglichen und zur neuen Datei
+# input_file = 'SMI_Gesamtboden_monatlich.nc'
+# output_file = 'filtered_SMI_Gesamtboden_monatlich.nc'
+
+# # Ausführen der Funktion
+# remove_months(input_file, output_file)
+
